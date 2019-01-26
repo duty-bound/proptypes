d29e6b473ee4df0775bd063c6bd93a3f4eaa0082
user <duty_bound@aol.com>
2019-01-26 16:20:32 +0100

    Done some modifications

--- hello.js
+++ hello.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import { date } from './get-date'
-export const Hello = (props) => {
-  console.log(props);
-  return(
+export const Hello = (props) =>
     <div>
       <h2>{props.title}</h2>
       <p>Date: <span>{props.date}/{props.month}/{props.year}</span></p>
       <p>Weekend: {props.isWeekend(props.day) ? 'yes' : 'no'}</p>
       <p>Single: {props.status ? 'single' : 'engaged'}</p>
-      <p>{props.oss.map(os => <span>{os} </span>)}</p>
-      <p>{Object.values(props.computer).map(value => <span>{value} </span>)}</p>
+      <p>{props.oss.map((os, i) => <span key={i}>{os} </span>)}</p>
+      <p>{Object.values(props.computer).map((value, i) => <span key={i}>{value} </span>)}</p>
     </div>
-  )
-}
